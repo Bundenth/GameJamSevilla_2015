176e18df888b94912d4199ab97fe2192d6ea31d3
Bundenth <carlos.fernandez.musoles@gmail.com>
2015-03-21 09:53:48 +0000

    Controlador para el bus y la camara

--- GameJam_Sevilla 2015/Assets/Scripts/CameraFollow.cs
+++ GameJam_Sevilla 2015/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,29 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
-	public float speed = 5f;
+	public float positionDamping = 3f;
+	public float angleDamping = 3f;
+
+	private Vector3 offset;
+	private float xAngle;
 
 	// Use this for initialization
 	void Start () {
-	
+		offset = target.position - transform.position;
+		xAngle = transform.eulerAngles.x;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.position = Vector3.Lerp (transform.position,target.position + Vector3.up*7f - Vector3.forward*1.4f,Time.deltaTime * speed);
+	void FixedUpdate () {
+		float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = target.transform.eulerAngles.y;
+		float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * angleDamping);
+		
+		Quaternion rotation = Quaternion.Euler(0, angle, 0);
+		transform.position = Vector3.Lerp (transform.position,target.transform.position - (rotation * offset),Time.deltaTime*positionDamping);
+		
+		transform.LookAt(target.transform);
+		transform.eulerAngles = new Vector3(xAngle,transform.eulerAngles.y,transform.eulerAngles.z);
+
 	}
 }
