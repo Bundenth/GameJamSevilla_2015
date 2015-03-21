921958ceff69336d1c6ce95fd614ac3e422d4faa
Bundenth <carlos.fernandez.musoles@gmail.com>
2015-03-21 22:17:48 +0000

    Debug and trafico_test

--- GameJam_Sevilla 2015/Assets/Scripts/Player.cs
+++ GameJam_Sevilla 2015/Assets/Scripts/Player.cs
@@ -11,6 +11,11 @@ public class Player : MonoBehaviour {
 	public Text hostagesText;
 	public Text rescuedText;
 
+	// ENDINGS
+	public GameObject gameOverPanel;
+	public GameObject winEnding1;
+	public GameObject winEnding2;
+
 	public float lowSpeedTime = 3f;
 	public float speedLimit = 50f;
 	public float dropOffHostageTopSpeed = 60f;
@@ -32,12 +37,17 @@ public class Player : MonoBehaviour {
 		health = MAX_HEALTH;
 		hostages = NUM_HOSTAGES;
 	}
-	
+
 	void Update() {
 		if(speedText) {
 			speedText.text = "Speed: " + car.CurrentSpeed*SPEED_CONVERSION;
-			if(car.CurrentSpeed*SPEED_CONVERSION < speedLimit) speedText.color = Color.red;
-			else speedText.color = Color.white;
+			if(car.CurrentSpeed*SPEED_CONVERSION < speedLimit) {
+				speedText.color = Color.red;
+			} else if(car.CurrentSpeed*SPEED_CONVERSION < dropOffHostageTopSpeed) {
+				speedText.color = Color.green;
+			} else {
+				speedText.color = Color.white;
+			}
 		}
 		if(hostagesText) hostagesText.text = "Hostages: " + hostages;
 		if(healthText) healthText.text = "Health: " + health;
@@ -52,9 +62,13 @@ public class Player : MonoBehaviour {
 		if(CheckSpeedLimit() || CheckHealth()) {
 			// DIE!
 			Debug.Log ("DEAD!");
+			gameOverPanel.SetActive(true);
+			gameObject.SetActive(false);
 		}
 		if(CheckHostages()) {
 			Debug.Log ("FINISHED!");
+			EndGame();
+			gameObject.SetActive(false);
 		}
 	}
 
@@ -82,17 +96,31 @@ public class Player : MonoBehaviour {
 
 	void ReleaseHostage() {
 		hostages--;
+		Debug.DrawRay(transform.position+Vector3.up*0.1f,transform.right,Color.red,rescueDistance);
 		if(car.CurrentSpeed * SPEED_CONVERSION <= dropOffHostageTopSpeed) {
 			RaycastHit hit;
-			if(Physics.SphereCast(transform.position,0.5f,transform.right,out hit,rescueDistance,(1 << LayerMask.NameToLayer("Checkpoint")))) {
-				Debug.Log (hit.collider.name);
-				Debug.Log ("Well done! Rescued");
+			if(Physics.Raycast(transform.position+Vector3.up*0.1f,transform.right,out hit,rescueDistance,(1 << LayerMask.NameToLayer("Checkpoint")))) {
+				Debug.Log ("Well done! Rescued " + hit.collider.name);
 				hit.collider.enabled = false;
 				rescued++;
 			} 
 		}
 	}
 
+	void EndGame() {
+		if(rescued >= 5) {
+			if(rescued > 8) {
+				// ONE ENDING
+				winEnding1.SetActive(true);
+			} else {
+				// SECOND ENDING
+				winEnding2.SetActive (true);
+			}
+		} else {
+			gameOverPanel.SetActive(true);
+		}
+	}
+	
 	public void ApplyDamage(float damage) {
 		health -= damage;
 		Debug.Log ("Damaged!");
---
+++ GameJam_Sevilla 2015/Assets/Scripts/UIListener.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIListener : MonoBehaviour {
+
+	public void OnRestartClicked() {
+		Application.LoadLevel (0);
+	}
+}
