081eb611c2d07ef560eee9f75ef9856519c05d3f
Bundenth <carlos.fernandez.musoles@gmail.com>
2015-03-21 11:44:29 +0000

    Navmesh vehicles

--- GameJam_Sevilla 2015/Assets/Scripts/PathFollower.cs
+++ GameJam_Sevilla 2015/Assets/Scripts/PathFollower.cs
@@ -7,14 +7,20 @@ public class PathFollower : MonoBehaviour {
 	public Transform[] pathPoints;
 
 	private NavMeshAgent agent;
+	private int nextIndex = 0;
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(pathPoints[0].position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		agent.SetDestination(pathPoints[0].position);
+		if(agent.remainingDistance <= agent.stoppingDistance) {
+			nextIndex++;
+			if(nextIndex >= pathPoints.Length) nextIndex = 0;
+			agent.SetDestination(pathPoints[nextIndex].position);
+		}
 	}
 }
